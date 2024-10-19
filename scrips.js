88d360d9dbc4d62ea5bde715e0c17032089f1dd2
nanoNNsk <sibthinon@gmail.com>
2024-10-19 11:10:55 +0700

    json celsius

--- scrips.js
+++ scrips.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ celsius }),
+          body: JSON.stringify({ "celsius":celsius }),
         });
 
         if (response.ok) {
